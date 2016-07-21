d78602cae0b88a6a797bad44fce994f0457b5b23
Robby Kim <robert.st.kim@gmail.com>
2016-07-21 13:47:30 -0400

    moving the pokemon list outside of the component

--- js/index.jsx
+++ js/index.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var pokemonList = [];
+
 var SearchBarContainer = createClass({
   getInitialState: function () {
     return {
